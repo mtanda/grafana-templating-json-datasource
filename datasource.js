1d5ac4fc1a0b868cfe53e9a0b1e89de35542f806
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2016-02-18 13:10:08 +0900

    migrate to support grafana 3.0

--- datasource.js
+++ datasource.js
@@ -2,65 +2,69 @@ define([
   'angular',
   'lodash',
   'plugins/templating_json/bower_components/jmespath.js/jmespath.js',
+  'app/plugins/sdk',
 ],
 function (angular, _, jmespath) {
   'use strict';
 
   /** @ngInject */
   function TemplatingJsonDatasource(instanceSettings, $q, backendSrv, templateSrv) {
-
-    this.type = 'templating_json';
+    this.type = instanceSettings.type;
     this.name = instanceSettings.name;
     this.url = instanceSettings.url;
     this.basicAuth = instanceSettings.basicAuth;
     this.withCredentials = instanceSettings.withCredentials;
-
-    this._request = function(method, url) {
-      var options = {
-        url: url,
-        method: method
-      };
-
-      if (this.basicAuth || this.withCredentials) {
-        options.withCredentials = true;
-      }
-      if (this.basicAuth) {
-        options.headers = {
-          'Authorization': this.basicAuth
-        };
-      }
-
-      return backendSrv.datasourceRequest(options);
-    };
-
-    this.metricFindQuery = function(query) {
-      if (!query) { return $q.when([]); }
-
-      try {
-        query = templateSrv.replace(query);
-      } catch (err) {
-        return $q.reject(err);
-      }
-
-      return this._request('GET', this.url).then(function(response) {
-        var result = jmespath.search(response.data, query);
-
-        if (_.isString(result)) {
-          return [{ text: result }];
-        } else if (_.isArray(result)) {
-          return _.map(result, function(r) { return { text: r }; });
-        } else {
-          throw new Error('invalid data');
-        }
-      });
-    };
-
-    this.testDatasource = function() {
-      return this._request('GET', this.url).then(function() {
-        return { status: 'success', message: 'Data source is working', title: 'Success' };
-      });
-    };
+    this.supportMetrics = true;
+    this.q = $q;
+    this.backendSrv = backendSrv;
+    this.templateSrv = templateSrv;
   }
+
+  TemplatingJsonDatasource.prototype._request = function(method, url) {
+    var options = {
+      url: url,
+      method: method
+    };
+
+    if (this.basicAuth || this.withCredentials) {
+      options.withCredentials = true;
+    }
+    if (this.basicAuth) {
+      options.headers = {
+        'Authorization': this.basicAuth
+      };
+    }
+
+    return this.backendSrv.datasourceRequest(options);
+  };
+
+  TemplatingJsonDatasource.prototype.metricFindQuery = function(query) {
+    if (!query) { return this.q.when([]); }
+
+    try {
+      query = this.templateSrv.replace(query);
+    } catch (err) {
+      return this.q.reject(err);
+    }
+
+    return this._request('GET', this.url).then(function(response) {
+      var result = jmespath.search(response.data, query);
+
+      if (_.isString(result)) {
+        return [{ text: result }];
+      } else if (_.isArray(result)) {
+        return _.map(result, function(r) { return { text: r }; });
+      } else {
+        throw new Error('invalid data');
+      }
+    });
+  };
+
+  TemplatingJsonDatasource.prototype.testDatasource = function() {
+    return this._request('GET', this.url).then(function() {
+      return { status: 'success', message: 'Data source is working', title: 'Success' };
+    });
+  };
 
   return TemplatingJsonDatasource;
 });
--- module.js
+++ module.js
@@ -4,12 +4,11 @@ define([
 function (TemplatingJsonDatasource) {
   'use strict';
 
-  function configView() {
-    return {templateUrl: 'public/plugins/templating_json/partials/config.html'};
-  }
+  var TemplatingJsonConfigCtrl = function() {};
+  TemplatingJsonConfigCtrl.templateUrl = 'partials/config.html';
 
   return {
     Datasource: TemplatingJsonDatasource,
-    configView: configView
+    ConfigCtrl: TemplatingJsonConfigCtrl
   };
 });
